0fd81bda0530fa8dcfd5384353ebae83a5fed3b4
Leydi <leydi.madrid6@gmail.com>
2022-08-29 17:09:39 -0500

    Se agrega texto condicional

--- src/components/Tareaspendientes.jsx
+++ src/components/Tareaspendientes.jsx
@@ -1,21 +1,23 @@
 import Tareas from "./Tareas";
 
-const Tareaspendientes = ({tareas}) => {
-
+const Tareaspendientes = ({ tareas }) => {
   return (
     <div className="md-w-1/2 lg:w-2/5 mx-5 mb-10 md:h-screen overflow-scroll">
-      <h1 className="font-black text-2xl text-center mb-4">
-        Tareas pendientes
-      </h1>
+      {tareas && tareas.length ? (
+        <>
+          <h1 className="font-black text-2xl text-center mb-4">
+            Tareas pendientes
+          </h1>
 
-      {tareas.map((tarea)=> {
-        return (
-          <Tareas 
-          key={tarea.id}
-          tarea={tarea}/>
-          )
-        })}
-
+          {tareas.map((tarea) => {
+            return <Tareas key={tareas.id} tarea={tarea} />;
+          })}
+        </>
+      ) : (
+        <h1 className="font-black text-2xl text-center mb-4">
+          No tengo tareas pendientes
+        </h1>
+      )}
     </div>
   );
 };
